5890f3a0b180ae32556028d0d878f75a70c8d401
Th1iago3 <140936706+Th1iago3@users.noreply.github.com>
2025-02-12 03:25:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1007,7 +1007,7 @@ def nomelv():
 
             if data.get('resultado') and len(data['resultado']) > 0:
                 if manage_module_usage(g.user_id, 'nomelv'):
-                    results = data['resultado']
+                    result = data['resultado']
                 else:
                     flash('Limite de uso atingido para NOME.', 'error')
             else:
